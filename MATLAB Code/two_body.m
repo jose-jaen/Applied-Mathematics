383fa22f8386da88b58d688a5f4893343637dc26
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-11-17 17:28:20 +0100

    Update two_body.m

--- MATLAB Code/two_body.m
+++ MATLAB Code/two_body.m
@@ -2,12 +2,12 @@
 e = 0.7; m1 = 1; m2 = 4;
 
 % Establish period 
-T = 2*pi./(1-e).^1.5; tspan = linspace(0,T,1000);
+T = 2*pi./(1-e).^1.5; tspan = linspace(0, T, 1000);
 options = odeset('RelTol', 1.e-6);
 
 % Solve differential equation
 % First, express 2nd order ODE as 1st order ODE
-[t,xyuv] = ode45(@(t, xyuv) two_body(xyuv), tspan,[-1 0, 0, sqrt(1 + e)], options);
+[t, xyuv] = ode45(@(t, xyuv) two_body(xyuv), tspan, [-1, 0, 0, sqrt(1 + e)], options);
 x = xyuv(:, 1); y = xyuv(:, 2);
 x1 = m2/(m1 + m2)*x; y1=m2/(m1 + m2)*y;
 x2 = -m1/(m1 + m2)*x; y2=-m1/(m1 + m2)*y;
@@ -17,11 +17,11 @@ x2 = -m1/(m1 + m2)*x; y2=-m1/(m1 + m2)*y;
 k = 0.1;
 R1 = k*(m1)^(1/3); R2=k*(m2)^(1/3); 
 theta = linspace(0, 2*pi); 
-figure; axis equal; hold on; set(gcf,'color','w');
+figure; axis equal; hold on; set(gcf, 'color', 'w');
 axis off; 
 xlim([-2,5]); ylim([-2.5, 2.5]);
-body_one = fill(R1*cos(theta) + x1(1), R1*sin(theta) + y1(1),'b'); 
-body_two = fill(R2*cos(theta) + x2(1), R2*sin(theta) + y2(1),'r'); 
+body_one = fill(R1*cos(theta) + x1(1), R1*sin(theta) + y1(1), 'b'); 
+body_two = fill(R2*cos(theta) + x2(1), R2*sin(theta) + y2(1), 'r'); 
 pause(1);
 
 % Iterate over a number of periods
