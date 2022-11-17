7d8610d8ad2953d6315ffcb14fad8c81bf8f559b
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2022-11-17 17:36:39 +0100

    Add files via upload

---
+++ Python Code/two_body.py
@@ -0,0 +1,26 @@
+# Required packages
+import numpy as np
+from math import pi
+from scipy.integrate import odeint
+
+# Define parameters for mass and eccentricity
+e, m1, m2 = 0.7, 1, 4
+
+# Establish period 
+T = np.divide(2*pi, np.power((1-e), 1.5))
+tspan = np.linspace(0, T, 1000)
+initial = [-1, 0, 0, np.sqrt(1 + e)]
+
+# Function to solve ODE
+def two_body_eq(xyuv, t):
+    x, y = xyuv[0], xyuv[1]
+    u, v = xyuv[2], xyuv[3]
+    r = np.sqrt(x**2 + y**2)
+    d_xyuv_dt = np.array([
+        [u], [v],
+        [np.divide(-x, r**3)],
+        [np.divide(-y, r**3)]])
+    return d_xyuv_dt
+
+# Solution to ODE
+y = odeint(two_body_eq, initial, tspan)
